7931c22d6a4e0277d07083cc05eca160f0377ba4
bragomat <bragomat@gmail.com>
2020-07-01 20:21:33 +0300

    add authorisation test

--- webtest.py
+++ webtest.py
@@ -1,24 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 import time
 
-driver = webdriver.Chrome("driver\chromedriver.exe")
-driver.get("https://music.yandex.ru/")
-#login_button = driver.find_element_by_xpath("/html/body/div[1]/div[5]/div/div[2]/a")
-#login_button.click()
-#time.sleep(5)
-#login = driver.find_element_by_id('passp-field-login')
-#login.clear()
-#login.send_keys('*****')
-#driver.find_element_by_xpath('//*[@id="root"]/div/div/div[2]/div/div/div[3]/div[2]/div/div/div[1]/form/div[3]/button[1]').click()
-#time.sleep(2)
-#password = driver.find_element_by_name('passwd')
-#password.send_keys('*******')
-#driver.find_element_by_xpath('//*[@id="root"]/div/div/div[2]/div/div/div[3]/div[2]/div/div/form/div[2]/button[1]').click()
-#time.sleep(2)
-#searchbox = driver.find_element_by_xpath('/html/body/div[1]/div[5]/div/div[1]/div[1]/div/div[1]/input')
-searchbox = driver.find_element_by_xpath('/html/body/div[1]/div[5]/div/div[1]/div[1]/div/div[1]/input')
-searchbox.send_keys('simon says')
-searchbox.send_keys(Keys.ENTER)
-time.sleep(2)
-driver.find_element_by_xpath('/html/body/div[1]/div[7]/div[2]/div/div/div[2]/div/div[3]/div[1]/div[3]/span/button').click()
+try:
+    driver = webdriver.Chrome("driver\chromedriver.exe")
+    driver.get("http://127.0.0.1:4444/CerediraTess.html")
+
+    username = driver.find_element_by_id("username")
+    username.send_keys("usr_1")
+    password = driver.find_element_by_id("password")
+    password.send_keys("1qaz@WSX")
+    submit = driver.find_element_by_id("updateCT")
+    submit.click()
+
+    select = Select(driver.find_element_by_id("ctScripts"))
+    check = select.first_selected_option.text
+
+    assert check == "Выберите скрипт для выполнения", "Authorisation check"
+
+finally:
+    time.sleep(10)
+    driver.quit()
