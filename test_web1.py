6bb3e088eb8eedef0d7c069d048070becee6dcd1
bragomat <bragomat@gmail.com>
2020-07-16 22:15:15 +0300

    test yield

--- test_web1.py
+++ test_web1.py
@@ -5,23 +5,22 @@ import time
 
 @pytest.fixture()
 def browser():
-    global driver
-    driver = webdriver.Chrome("driver/chromedriver.exe")
-    driver.get("http://127.0.0.1:4444/CerediraTess.html")
-    driver.maximize_window()
-    yield
+    browser = webdriver.Chrome("driver/chromedriver.exe")
+    browser.get("http://127.0.0.1:4444/CerediraTess.html")
+    browser.maximize_window()
+    yield browser
     time.sleep(2)
-    driver.quit()
+    browser.quit()
 
 def test_auth(browser):
-    username = driver.find_element_by_id("username")
+    username = browser.find_element_by_id("username")
     username.send_keys("usr_1")
-    password = driver.find_element_by_id("password")
+    password = browser.find_element_by_id("password")
     password.send_keys("1qaz@WSX")
-    submit = driver.find_element_by_id("updateCT")
+    submit = browser.find_element_by_id("updateCT")
     submit.click()
 
-    select = Select(driver.find_element_by_id("ctScripts"))
+    select = Select(browser.find_element_by_id("ctScripts"))
     check = select.first_selected_option.text
 
     assert check == "Выберите скрипт для выполнения", "Authorisation check"
