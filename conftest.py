63b3c857904b6a10deb8493470d65f54c2ebb155
Демшин Дмитрий Олегович <Demshin_DO@pf.vis.local>
2020-08-07 10:43:48 +0300

    add support of firefox and ms edge browsers

--- conftest.py
+++ conftest.py
@@ -3,9 +3,22 @@ from selenium import webdriver
 
 link = "http://127.0.0.1:4444/CerediraTess.html"
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
 @pytest.fixture()
-def driver():
-    driver = webdriver.Chrome("driver/chromedriver.exe")
+def driver(request):
+    driver_name = request.config.getoption("browser_name")
+    driver = None
+    if driver_name == "chrome":
+        driver = webdriver.Chrome("driver/chromedriver.exe")
+    elif driver_name == "firefox":
+        driver = webdriver.Firefox(executable_path="driver/geckodriver.exe")
+    elif driver_name == "edge":
+        driver = webdriver.Edge("driver/msedgedriver.exe")
+    else:
+        raise pytest.UsageError("--browser_name should be chrome, firefox or edge")
     driver.get(link)
     driver.maximize_window()
     yield driver
